4cbe45498bad43be69b495195c3b220d029db37e
LieutPaul <Vikas.Kalyanapuram@iiitb.ac.in>
2023-02-15 16:27:59 +0530

    Maintaining inputted value in NavBar

--- src/components/FlashcardList.jsx
+++ src/components/FlashcardList.jsx
@@ -5,9 +5,9 @@ export default function FlashcardList({flashCards}){
   return (
     <div className='container-fluid'>
       <div className='row'>
-        {flashCards.map((flashCard)=>{
+        {flashCards.map((flashCard,number)=>{
           return (
-            <div key={flashCard.id} className="col-lg-3 col-md-4 col-sm-6 col-xs-12 mb-4 mt-4">
+            <div key={number} className="col-lg-3 col-md-4 col-sm-6 col-xs-12 mb-4 mt-4">
               <Flashcard flashCard={flashCard}/>
             </div>
           );
